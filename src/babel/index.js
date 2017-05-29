6782f17a3f9b3eeb05114155c19883dd3b5d3d0e
Paweł Trysła <pawel.trysla@gmail.com>
2017-05-29 16:51:13 +0200

    tweaks

--- src/babel/index.js
+++ src/babel/index.js
@@ -1,5 +1,4 @@
-const requireWithInterOp = require('./requireWithInterOp');
-const slugify = requireWithInterOp('../build/slugify');
+import slugify from '../slugify';
 
 const computeClassName = (name, taggedTemplateExpr) => {
   const classString = taggedTemplateExpr.quasi.quasis
@@ -7,27 +6,10 @@ const computeClassName = (name, taggedTemplateExpr) => {
       return acc.concat(quasi.value.cooked);
     }, '')
     .replace(/(^\s*|\s*$|\s{2,})/g, '');
-  debugger;
   return `${name}_${slugify(classString)}`;
 };
 
-// const getModulesToRequire = (t, taggedTemplateExpression) =>
-//   taggedTemplateExpression.quasi.expressions.filter(
-//     expr => t.isMemberExpression(expr) || t.isIdentifier(expr)
-//   );
-
-// const requireModules = (t, astBody, modulesToRequire) => {
-//   modulesToRequire.forEach(moduleName => {
-//     const importDecl = astBody.find(node => {
-//       if (!t.isImportDeclaration(node)) {
-//         return false;
-//       }
-//       debugger;
-//     });
-//   });
-// };
-
-module.exports = ({ types: t }) => ({
+export default ({ types: t }) => ({
   visitor: {
     VariableDeclarator(path) {
       if (
@@ -36,11 +18,6 @@ module.exports = ({ types: t }) => ({
         path.node.init.tag.name === 'css'
       ) {
         const taggedTemplateExpression = path.node.init;
-        // const modulesBody = requireModules(
-        //   t,
-        //   this.file.ast.program.body,
-        //   getModulesToRequire(t, taggedTemplateExpression)
-        // );
 
         if (taggedTemplateExpression.quasi.expressions.length) {
           throw new Error(
