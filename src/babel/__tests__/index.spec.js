7eb295dfde045fc885bc4bb73f2244ecbae5757b
Paweł Trysła <pawel.trysla@gmail.com>
2017-08-08 11:59:34 +0200

    add test suite for babel plugin

--- src/babel/__tests__/index.spec.js
+++ src/babel/__tests__/index.spec.js
@@ -2,16 +2,41 @@ jest.mock('../extractStyles', () => ({ __esModule: true, default: jest.fn() }));
 
 import * as babel from 'babel-core';
 import path from 'path';
-import extractStyles from '../extractStyles';
 import dedent from 'dedent';
 
-const transpile = source => {
-  return babel.transform(source, {
-    presets: ['es2015'],
-    plugins: [path.resolve('src/babel/index.js')],
-    babelrc: false,
-  });
-};
+function transpile(source) {
+  const { code } = babel.transform(
+    dedent`
+      import css from './build/css';
+
+      ${source}
+
+      /* results */
+      const results = preval\`
+        import { getCache } from './build/css';
+        module.exports = getCache();
+      \`;
+    `,
+    {
+      presets: ['es2015'],
+      plugins: [
+        path.resolve('src/babel/index.js'),
+        require.resolve('babel-plugin-preval'),
+      ],
+      babelrc: false,
+    }
+  );
+
+  const splitIndex = code.indexOf('/* results */');
+  return {
+    code: code.substr(0, splitIndex),
+    results: eval(`${code.substr(splitIndex)}; results;`),
+  };
+}
+
+function filterResults(results, match) {
+  return results.find(e => e.selector.includes(match[1])) || {};
+}
 
 describe('babel plugin', () => {
   it('should not process tagged template if tag is not `css`', () => {
@@ -24,206 +49,137 @@ describe('babel plugin', () => {
     expect(code).toMatchSnapshot();
   });
 
-  it('should fail if there are any unresolvable expressions in tagged template', () => {
+  it('should throw error if `css` tagged template literal is not assigned to a variable', () => {
     expect(() => {
-      transpile(
-        dedent(`
-        const header = css\`
-          font-size: \${someFn()}em;
-        \`;
-        `)
-      );
-    }).toThrowError(/Cannot resolve expression/);
+      transpile(dedent`
+      css\`
+        font-size: 3em;
+      \`;
+      `);
+    }).toThrow();
   });
 
-  it('should generate hash and inject it into `css.named` call for simple fixture', () => {
-    const { code } = transpile(dedent`
+  it('should create classname for `css` tagged template literal', () => {
+    const { code, results } = transpile(dedent`
     const header = css\`
       font-size: 3em;
     \`;
     `);
-    expect(code.includes('header_')).toBeTruthy();
-    expect(code).toMatchSnapshot();
-    // Check if generated hash is the same
-    expect(
-      transpile(
-        dedent(`
-        const header = css\`
-          font-size: 3em;
-        \`;
-        `)
-      ).code
-    ).toMatchSnapshot();
+
+    const match = /header = "(header_[a-z0-9]+)"/g.exec(code);
+    expect(match).not.toBeNull();
+    const { styles } = filterResults(results, match);
+    expect(styles).toMatch('font-size: 3em');
+    expect(styles).toMatchSnapshot();
   });
 
-  it('should generate hash and inject it into `css.named` call for multiline fixture', () => {
-    const { code } = transpile(dedent`
+  it('should create classnames for multiple `css` tagged template literal', () => {
+    const { code, results } = transpile(dedent`
     const header = css\`
-      font-size: 3em;
-      color: red;
-      border: 1px solid #000;
+      font-size: ${'${2 + 1}'}em;
+    \`;
+
+    const body = css\`
+      border-radius: ${'${2 + 2}'}px;
     \`;
     `);
-    expect(code.includes('header_')).toBeTruthy();
-    expect(code).toMatchSnapshot();
-    // Check if generated hash is the same
-    expect(
-      transpile(
-        dedent(`
-        const header = css\`
-          font-size: 3em;
-          color: red;
-          border: 1px solid #000;
-        \`;
-        `)
-      ).code
-    ).toMatchSnapshot();
+
+    const headerMatch = /header = "(header_[a-z0-9]+)"/g.exec(code);
+    const bodyMatch = /body = "(body_[a-z0-9]+)"/g.exec(code);
+    expect(headerMatch).not.toBeNull();
+    expect(bodyMatch).not.toBeNull();
+    const { styles: headerStyles } = filterResults(results, headerMatch);
+    const { styles: bodyStyles } = filterResults(results, bodyMatch);
+    expect(headerStyles).toMatch('font-size: 3em');
+    expect(headerStyles).toMatchSnapshot();
+    expect(bodyStyles).toMatch('border-radius: 4px');
+    expect(bodyStyles).toMatchSnapshot();
   });
 
-  it('should resolve expressions and generate hash (CommonJS)', () => {
-    extractStyles.mockImplementationOnce(tagExpr => {
-      expect(tagExpr.quasi.expressions.length).toBe(0);
-      expect(
-        tagExpr.quasi.quasis[0].value.cooked.includes('font-size: 14px')
-      ).toBeTruthy();
-    });
-    const { code } = transpile(
-      dedent(`
-      const constants = require('./src/babel/__tests__/__fixtures__/constants.cjs.js');
-
-      const header = css\`
-        font-size: \${constants.fontSize};
-        color: black;
-      \`;
-      `)
-    );
-    expect(code).toMatchSnapshot();
-  });
-
-  it('should resolve expressions and generate hash (ESM)', () => {
-    extractStyles.mockImplementationOnce(tagExpr => {
-      expect(tagExpr.quasi.expressions.length).toBe(0);
-      expect(
-        tagExpr.quasi.quasis[0].value.cooked.includes('font-size: 14px')
-      ).toBeTruthy();
-      expect(
-        tagExpr.quasi.quasis[0].value.cooked.includes('color: black')
-      ).toBeTruthy();
-    });
-    const { code } = transpile(
-      dedent(`
-      import constants from './src/babel/__tests__/__fixtures__/constants.esm.js';
-
-      const header = css\`
-        font-size: \${constants.fontSize};
-        color: black;
-      \`;
-      `)
-    );
-    expect(code).toMatchSnapshot();
-  });
-
-  it('should resolve expressions from an object with constants', () => {
-    extractStyles.mockImplementationOnce(tagExpr => {
-      expect(tagExpr.quasi.expressions.length).toBe(0);
-      expect(
-        tagExpr.quasi.quasis[0].value.cooked.includes('font-size: 14px')
-      ).toBeTruthy();
-      expect(
-        tagExpr.quasi.quasis[0].value.cooked.includes('color: black')
-      ).toBeTruthy();
-    });
-    const { code } = transpile(
-      dedent(`
-      const constants = { fontSize: '14px', color: 'black' };
-
-      const header = css\`
-        font-size: \${constants.fontSize};
-        color: \${constants.color};
-      \`;
-      `)
-    );
-    expect(code).toMatchSnapshot();
-  });
-
-  it('should resolve expressions from an object with constants with destructurization', () => {
-    extractStyles.mockImplementationOnce(tagExpr => {
-      expect(tagExpr.quasi.expressions.length).toBe(0);
-      expect(
-        tagExpr.quasi.quasis[0].value.cooked.includes('font-size: 14px')
-      ).toBeTruthy();
-      expect(
-        tagExpr.quasi.quasis[0].value.cooked.includes('color: black')
-      ).toBeTruthy();
-    });
-    const { code } = transpile(
-      dedent(`
-      const { fontSize, color } = { fontSize: '14px', color: 'black' };
-
-      const header = css\`
-        font-size: \${fontSize};
-        color: \${color};
-      \`;
-      `)
-    );
-    expect(code).toMatchSnapshot();
-  });
-
-  it('should resolve expressions from an object with constants with advanced destructurization', () => {
-    extractStyles.mockImplementationOnce(tagExpr => {
-      expect(tagExpr.quasi.expressions.length).toBe(0);
-      expect(
-        tagExpr.quasi.quasis[0].value.cooked.includes('font-size: 14px')
-      ).toBeTruthy();
-      expect(
-        tagExpr.quasi.quasis[0].value.cooked.includes('color: black')
-      ).toBeTruthy();
-    });
-    const { code } = transpile(
-      dedent(`
-      const { fontSize = '14px', color: fontColor } = { color: 'black' };
-
-      const header = css\`
-        font-size: \${fontSize};
-        color: \${fontColor};
-      \`;
-      `)
-    );
-    expect(code).toMatchSnapshot();
-  });
-  it('should resolve multiple expressions', () => {
-    extractStyles.mockImplementationOnce(tagExpr => {
-      expect(tagExpr.quasi.expressions.length).toBe(0);
-      expect(
-        tagExpr.quasi.quasis[0].value.cooked.includes('font-size: 14px')
-      ).toBeTruthy();
-      expect(
-        tagExpr.quasi.quasis[0].value.cooked.includes('color: black')
-      ).toBeTruthy();
-      expect(
-        tagExpr.quasi.quasis[0].value.cooked.includes('background-color: black')
-      ).toBeTruthy();
-      expect(
-        tagExpr.quasi.quasis[0].value.cooked.includes('font-weight: bold')
-      ).toBeTruthy();
-    });
-    const { code } = transpile(
-      dedent(`
+  describe('with plain objects', () => {
+    it('should preval styles with shallow object', () => {
+      const { code, results } = transpile(dedent`
       const constants = {
-        color: 'black',
-        fontSize: '14px',
-        backgroundColor: 'black',
-        fontWeight: 'bold',
+        fontSize: '3em',
       };
 
       const header = css\`
-        font-size: \${constants.fontSize};
-        color: \${constants.color};
-        background-color: \${constants.backgroundColor};
-        font-weight: \${constants.fontWeight};
+        font-size: ${'${constants.fontSize}'};
       \`;
-      `)
-    );
-    expect(code).toMatchSnapshot();
+      `);
+
+      const match = /header = "(header_[a-z0-9]+)"/g.exec(code);
+      expect(match).not.toBeNull();
+      const { styles } = filterResults(results, match);
+      expect(styles).toMatch('font-size: 3em');
+      expect(styles).toMatchSnapshot();
+    });
+
+    it('should preval styles with nested object', () => {
+      const { code, results } = transpile(dedent`
+      const constants = {
+        header: {
+          default: {
+            font: {
+              size: '3em',
+            },
+          },
+        },
+      };
+
+      const header = css\`
+        font-size: ${'${constants.header.default.font.size}'};
+      \`;
+      `);
+
+      const match = /header = "(header_[a-z0-9]+)"/g.exec(code);
+      expect(match).not.toBeNull();
+      const { styles } = filterResults(results, match);
+      expect(styles).toMatch('font-size: 3em');
+      expect(styles).toMatchSnapshot();
+    });
+
+    it('shoyld preval styles with destructured object', () => {
+      expect(() => {
+        transpile(dedent`
+        const { base } = {
+          base: {
+            font: {
+              size: '3em',
+            },
+          },
+        };
+
+        const header = css\`
+          font-size: ${'${base.font.size}'};
+        \`;
+        `);
+      }).toThrow();
+
+      // Uncomment once we support destructurisation
+      // const match = /header = "(header_[a-z0-9]+)"/g.exec(code);
+      // expect(match).not.toBeNull();
+      // const { styles } = filterResults(results, match);
+      // expect(styles).toMatch('font-size: 3em');
+      // expect(styles).toMatchSnapshot();
+    });
+  });
+
+  describe('with commonjs imports', () => {
+    it('should preval imported constants ', () => {
+      const { code, results } = transpile(dedent`
+      const constants = require('./src/babel/__tests__/__fixtures__/commonjs/constants.js');
+
+      const header = css\`
+        font-size: ${'${constants.fontSize}'};
+      \`;
+      `);
+
+      const match = /header = "(header_[a-z0-9]+)"/g.exec(code);
+      expect(match).not.toBeNull();
+      const { styles } = filterResults(results, match);
+      expect(styles).toMatch('font-size: 14px');
+      expect(styles).toMatchSnapshot();
+    });
   });
 });
