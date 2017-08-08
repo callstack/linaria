1331c8f7f96ab06f7c95abd12977330c71882970
Paweł Trysła <pawel.trysla@gmail.com>
2017-08-08 11:58:49 +0200

    temporarily add cache to css

--- src/css.js
+++ src/css.js
@@ -4,6 +4,7 @@ import slugify from './slugify';
 import sheet from './sheet';
 
 const all = sheet();
+const cache = [];
 
 const named = (id?: string, createSlug?: boolean = false) => (
   template: string[],
@@ -19,6 +20,7 @@ const named = (id?: string, createSlug?: boolean = false) => (
   const selector = `.${slug}`;
 
   all.insert(selector, styles);
+  cache.push({ selector, styles });
 
   return slug;
 };
@@ -29,3 +31,5 @@ css.title = id => named(id, true);
 css.named = named;
 
 export default css;
+
+export const getCache = () => cache;
