a0d9416035ce9a922d815526d7ebf8b0bf21475b
Satyajit Sahoo <satyajit.happy@gmail.com>
2017-08-26 22:26:08 +0200

    BREAKING: drop compose It's tricky to make compose work without introducing a lot of CSS manipulation in runtime. It also makes it difficult to make hot reload work. It's not a blocker for me and we should drop it to speed up the release.
    If anyone has ideas one how to handle specificity issues, I'd love to chat.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /* @flow */
 
 export { default as css } from './css';
-export { default as compose } from './compose';
+export { default as names } from './names';
 export { default as slugify } from './slugify';
