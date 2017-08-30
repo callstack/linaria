49f3bc01e724fc2d7abda41b34b2619987c4c4b2
Paweł Trysła <pawel.trysla@gmail.com>
2017-08-30 12:05:33 +0200

    use require.resolve to check if import is to linaria

--- src/babel/rewire-imports/index.js
+++ src/babel/rewire-imports/index.js
@@ -6,7 +6,9 @@ type State = {
   shouldSkip: boolean,
 };
 
-const linariaImportRegex = /linaria(?!\/build)/;
+function isLinariaImport(value) {
+  return require.resolve(value) === require.resolve('linaria');
+}
 
 export default ({ types }: { types: BabelTypes }) => ({
   visitor: {
@@ -19,10 +21,7 @@ export default ({ types }: { types: BabelTypes }) => ({
       },
     },
     ImportDeclaration(path: NodePath<any>, state: State) {
-      if (
-        !state.shouldSkip &&
-        linariaImportRegex.test(path.node.source.value)
-      ) {
+      if (!state.shouldSkip && isLinariaImport(path.node.source.value)) {
         path.node.source.value = `${path.node.source
           .value}/build/index.runtime.js`.replace(/\/\//g, '/');
       }
@@ -35,7 +34,7 @@ export default ({ types }: { types: BabelTypes }) => ({
         !state.shouldSkip &&
         types.isCallExpression(path.node.init) &&
         path.node.init.callee.name === 'require' &&
-        linariaImportRegex.test(path.node.init.arguments[0].value)
+        isLinariaImport(path.node.init.arguments[0].value)
       ) {
         // @TODO: it's very tricky to implement this for require
         throw path.buildCodeFrameError(
