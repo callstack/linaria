820001568278111fe1fb5689eaa152a8cffb81d0
Satyajit Sahoo <satyajit.happy@gmail.com>
2017-09-11 20:42:22 +0200

    refactor: use single implementation of sheet (#109)

---
+++ src/__tests__/sheet.spec.js
@@ -0,0 +1,54 @@
+/* @flow */
+
+import dedent from 'dedent';
+import document from '../document';
+import sheet from '../sheet';
+
+beforeEach(sheet.dump);
+
+describe('sheet module', () => {
+  it('should insert a stylesheet', () => {
+    expect(document.head.children[0].constructor.name).toBe('HTMLStyleElement');
+    expect(document.head.children[0].textContent).toEqual('');
+  });
+
+  it('should insert styles', () => {
+    sheet.insert('.some_selector', '{color:blue}');
+    sheet.insert('.foo', '{color:purple;font-size:12px;}');
+    expect(document.head.children[0].textContent.trim()).toBe(dedent`
+      .some_selector{color:blue;}
+      .foo{color:purple;font-size:12px;}
+    `);
+    expect(document.head.children[0].textContent.trim()).toBe(dedent`
+      .some_selector{color:blue;}
+      .foo{color:purple;font-size:12px;}
+    `);
+  });
+
+  it('should give styles list', () => {
+    sheet.insert('some_selector', '{color:rebeccapurple}');
+    expect(sheet.styles()).toEqual({
+      some_selector: '{color:rebeccapurple}',
+    });
+  });
+
+  it('should get styles with dump', () => {
+    sheet.insert('some_selector', '{color:blue}');
+    expect(sheet.dump()).toBe('some_selector{color:blue;}');
+  });
+
+  it('should clear cache on dump', () => {
+    sheet.insert('.foo', '{color:palevioletred}');
+    expect(sheet.styles()).toEqual({ '.foo': '{color:palevioletred}' });
+    sheet.dump();
+    expect(sheet.dump()).toBe('');
+    expect(sheet.styles()).toEqual({});
+  });
+
+  it('should not insert same css multiple times', () => {
+    sheet.insert('.lol', '{color:pink}');
+    sheet.insert('.lol', '{color:pink}');
+
+    expect(sheet.dump()).toBe('.lol{color:pink;}');
+  });
+});
--- src/sheet.js
+++ src/sheet.js
@@ -3,39 +3,12 @@
 import stylis from 'stylis';
 
 function sheet() {
-  let ruleCache: ?Array<{
-    selectorText: string,
-    cssText: string,
-    style: Object,
-  }>;
   let cache: { [selector: string]: string } = {};
-  let cssText = null;
 
-  if (typeof window === 'undefined' || typeof window.document === 'undefined') {
-    return {
-      insert(selector: string, css: string) {
-        if (selector in cache) {
-          return;
-        }
-
-        const text = stylis(selector, css);
-        cache[selector] = css;
-        cssText = cssText ? `${cssText}\n${text}` : text;
-      },
-      rules() {
-        throw new Error('Not implemented');
-      },
-      styles() {
-        return cache;
-      },
-      dump() {
-        const result = cssText || '';
-        cssText = null;
-        cache = {};
-        return result;
-      },
-    };
-  }
+  const isBrowser =
+    typeof window === 'object' && window != null && window.document != null;
+  // eslint-disable-next-line global-require
+  const document = isBrowser ? window.document : require('./document').default;
 
   const style = document.createElement('style');
   const node = document.createTextNode('');
@@ -43,13 +16,9 @@ function sheet() {
   style.appendChild(node);
   style.setAttribute('type', 'text/css');
 
-  if (document.head != null) {
-    document.head.appendChild(style);
-  } else {
-    throw new Error('Unable to insert stylesheet');
-  }
+  document.head.appendChild(style);
 
-  if (process.env.NODE_ENV !== 'test') {
+  if (process.env.NODE_ENV !== 'test' && isBrowser) {
     console.warn(
       'Babel preset for Linaria is not configured. See https://github.com/callstack-io/linaria/blob/master/docs/BABEL_PRESET.md for instructions.'
     );
@@ -64,26 +33,15 @@ function sheet() {
       const text = stylis(selector, css);
       cache[selector] = css;
       node.appendData(`\n${text}`);
-      // invalidate the cache since stylesheets have changed
-      ruleCache = null;
-    },
-    rules() {
-      if (ruleCache != null) {
-        return { list: ruleCache, cache: true };
-      }
-
-      ruleCache = [].concat(
-        /* $FlowFixMe */
-        ...Array.from(document.styleSheets).map(s => Array.from(s.cssRules))
-      );
-
-      return { list: ruleCache, cache: false };
     },
     styles() {
       return cache;
     },
     dump() {
-      throw new Error('Not implemented');
+      const result = node.textContent;
+      cache = {};
+      node.textContent = '';
+      return result.trim();
     },
   };
 }
