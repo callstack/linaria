fe831c1e26e0c9b12e2ae0de663a4927e96d6f4f
Satyajit Sahoo <satyajit.happy@gmail.com>
2017-09-24 20:50:14 +0200

    feat: add a hoist-css plugin. resolves #110

--- babel.js
+++ babel.js
@@ -3,6 +3,7 @@
 module.exports = function linariaBabelPreset(context, opts = {}) {
   return {
     plugins: [
+      [require('./build/babel/hoist-css').default, opts],
       [require('./build/babel/preval-extract').default, opts],
       [require('./build/babel/rewire-imports').default, opts],
     ],
