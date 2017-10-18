327ab71cf00b8db080f42da0cf3051a23bf14ed0
Paweł Trysła <pawel.trysla@gmail.com>
2017-10-18 16:46:49 +0200

    add linaria stylelint config

---
+++ stylelint-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: 'stylelint-config-recommended',
+  rules: {
+    'at-rule-name-case': 'lower',
+    'unit-case': 'lower',
+    'at-rule-semicolon-newline-after': 'always',
+    'max-empty-lines': 1,
+    'no-empty-source': null,
+    'length-zero-no-unit': true,
+    'no-extra-semicolons': null,
+  },
+};
