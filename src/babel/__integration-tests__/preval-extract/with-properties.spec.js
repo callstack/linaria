59d460440a7d9e20060ec3f8571906f65e464acd
Paweł Trysła <pawel.trysla@gmail.com>
2017-10-23 12:32:21 +0200

    add test cases to ensure class names are unique

--- src/babel/__integration-tests__/preval-extract/with-properties.spec.js
+++ src/babel/__integration-tests__/preval-extract/with-properties.spec.js
@@ -4,7 +4,27 @@
 import dedent from 'dedent';
 import { transpile } from '../__utils__/exec';
 
-describe('with object and jsx properties', () => {
+function assertAndCheckForDuplications(
+  { code, getCSSForClassName },
+  regex,
+  count,
+  stylesToMatch
+) {
+  const classNames = [];
+  for (let i = 0; i < count; i++) {
+    const match = regex.exec(code);
+
+    expect(match).not.toBeNull();
+    expect(classNames.indexOf(match[1])).toBe(-1);
+
+    classNames.push(match[1]);
+
+    const css = getCSSForClassName(match[1]);
+    expect(css).toMatch(stylesToMatch);
+  }
+}
+
+describe('with object and JSX properties', () => {
   it('should preval with object properties', () => {
     const { code, getCSSForClassName } = transpile(dedent`
       const styles = {
@@ -18,6 +38,37 @@ describe('with object and jsx properties', () => {
     expect(match).not.toBeNull();
     const css = getCSSForClassName(match[1]);
     expect(css).toMatch('font-size: 3em');
+  });
+
+  it('should not generate duplicated class names with object properties', () => {
+    const regex = /\/\*.+\*\/'(_header(\d)?__[a-z0-9]+)'/g;
+    assertAndCheckForDuplications(
+      transpile(dedent`
+        const header = css\`
+          font-size: 3em;
+        \`;
+
+        const styles = {
+          header: css\`
+            font-size: 3em;
+          \`,
+        };
+
+        const base = {
+          header: css\`
+            font-size: 3em;
+          \`,
+          nested: {
+            header: css\`
+              font-size: 3em;
+            \`,
+          }
+        };
+      `),
+      regex,
+      4,
+      'font-size: 3em'
+    );
   });
 
   it('should preval with JSX props', () => {
@@ -40,5 +91,33 @@ describe('with object and jsx properties', () => {
     expect(match).not.toBeNull();
     const css = getCSSForClassName(match[1]);
     expect(css).toMatch('font-size: 3em');
+  });
+
+  it('should not generate duplicated class names with JSX', () => {
+    const regex = /\/\*.+\*\/'(_article(\d)?__[a-z0-9]+)'/g;
+    assertAndCheckForDuplications(
+      transpile(dedent`
+      const article = css\`
+        font-size: 3em;
+      \`;
+
+      const styles = {
+        article: css\`
+          font-size: 3em;
+        \`,
+      };
+
+        <div>
+          <article {...styles(css\`font-size: 3em\`)} />
+          <article {...styles(css\`font-size: 3em\`)} />
+          <div>
+            <article {...styles(css\`font-size: 3em\`)} />
+          </div>
+        </div>
+      `),
+      regex,
+      5,
+      'font-size: 3em'
+    );
   });
 });
