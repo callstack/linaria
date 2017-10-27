cb706a4b41e40643c246a89e6e66f3c1e02f546c
Paweł Trysła <pawel.trysla@gmail.com>
2017-10-27 14:33:19 +0200

    remove unnecessary rules from stylelint config

--- stylelint-config.js
+++ stylelint-config.js
@@ -5,10 +5,7 @@ module.exports = {
   rules: {
     'at-rule-name-case': 'lower',
     'unit-case': 'lower',
-    'at-rule-semicolon-newline-after': 'always',
-    'max-empty-lines': 1,
     'no-empty-source': null,
-    'length-zero-no-unit': true,
     'no-extra-semicolons': null,
   },
 };
