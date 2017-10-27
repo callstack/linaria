522a5affd9bb4733dd9a3c23d8b64e7e63793bac
Paweł Trysła <pawel.trysla@gmail.com>
2017-10-27 11:55:07 +0200

    fix errors

--- babel.js
+++ babel.js
@@ -9,8 +9,8 @@ module.exports = function linariaBabelPreset(context, opts = {}) {
 
   return {
     plugins: [
-      [require('./build/babel/preval-extract').default, opts],
-      [require('./build/babel/rewire-imports').default, opts],
+      [require('./build/babel/preval-extract').default, options],
+      [require('./build/babel/rewire-imports').default, options],
     ],
   };
 };
