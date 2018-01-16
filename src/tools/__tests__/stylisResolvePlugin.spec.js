ecfe7c9a0e062bbe659178696ec5520ee013ad71
Paweł Trysła <pawel.trysla@gmail.com>
2018-01-16 15:44:28 +0100

    provide @linaria/env module dynamically for stylis resolve plugin

---
+++ src/tools/__tests__/stylisResolvePlugin.spec.js
@@ -0,0 +1,58 @@
+import path from 'path';
+import stylisResolvePlugin from '../stylisResolvePlugin';
+import env from '@linaria/env'; // eslint-disable-line
+
+jest.mock('@linaria/env', () => ({ config: {} }), { virtual: true });
+jest.mock('glob', () => ({
+  // eslint-disable-next-line global-require
+  sync: jest.fn(() => require('path').resolve('static/Image.jpg')),
+}));
+
+describe('stylis resolve plugin', () => {
+  beforeEach(() => {
+    env.config.single = undefined;
+    env.config.outDir = undefined;
+    env.config.filename = undefined;
+    env.filename = path.resolve('src/components/Container.js');
+  });
+
+  it('should do nothing', () => {
+    expect(stylisResolvePlugin(2, "url('abc')")).toEqual("url('abc')");
+    expect(stylisResolvePlugin(1, 'background:none')).toEqual(
+      'background:none'
+    );
+    expect(
+      stylisResolvePlugin(
+        1,
+        `background:url('${path.resolve('static/Image.jpg')}')`
+      )
+    ).toEqual(`background:url('${path.resolve('static/Image.jpg')}')`);
+  });
+
+  it('should resolve correct path without custom config', () => {
+    expect(
+      stylisResolvePlugin(1, "background:url('../../static/Image.jpg')")
+    ).toEqual("background:url('../../../static/Image.jpg')");
+
+    env.filename = 'src/components/Container.js';
+    expect(
+      stylisResolvePlugin(1, "background:url('../../static/Image.jpg')")
+    ).toEqual("background:url('../../../static/Image.jpg')");
+  });
+
+  it('should resolve correct path with custom `outDir`', () => {
+    env.config.outDir = 'node_modules/.cache/linaria';
+    expect(
+      stylisResolvePlugin(1, "background:url('../../static/Image.jpg')")
+    ).toEqual("background:url('../../../../../static/Image.jpg')");
+  });
+
+  it('should resolve correct path with custom `single: true` and custom `outDir`', () => {
+    env.config.single = true;
+    env.config.filename = 'styles.css';
+    env.config.outDir = 'dist';
+    expect(
+      stylisResolvePlugin(1, "background:url('../../static/Image.jpg')")
+    ).toEqual("background:url('../static/Image.jpg')");
+  });
+});
---
+++ src/tools/stylisResolvePlugin.js
@@ -0,0 +1,45 @@
+/* @flow */
+
+import path from 'path';
+import glob from 'glob';
+
+const files = glob.sync('**/*', {
+  ignore: ['node_modules/**/*'],
+  absolute: true,
+});
+
+function resolvePath(original: string) {
+  const {
+    config: { single, outDir, filename: cssFilename },
+    filename,
+    // $FlowFixMe
+  } = require('@linaria/env'); // eslint-disable-line
+
+  const absAssetFilename = path.join(path.dirname(filename), original);
+  const relativeFilename = path.isAbsolute(filename)
+    ? path.relative(process.cwd(), filename)
+    : filename;
+  const absCssFilename = path.resolve(
+    outDir || '.linaria-cache',
+    single ? cssFilename : relativeFilename
+  );
+
+  if (
+    path.isAbsolute(original) ||
+    files.indexOf(path.resolve(path.dirname(absCssFilename), original)) > -1
+  ) {
+    return original;
+  }
+
+  return path.relative(path.dirname(absCssFilename), absAssetFilename);
+}
+
+function replacePath(input: string, match: string) {
+  return input.replace(match, resolvePath(match));
+}
+
+export default function stylisResolvePlugin(context: number, content: string) {
+  return context === 1 // Apply on property
+    ? content.replace(/url\(['"](.+)['"]\)/, replacePath)
+    : content;
+}
