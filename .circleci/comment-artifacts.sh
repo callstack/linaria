f713b33573b5e7c79454627de58594f9874861fd
Michał Pierzchała <thymikee@gmail.com>
2018-02-02 22:02:37 +0100

    End sentence with a dot

--- .circleci/comment-artifacts.sh
+++ .circleci/comment-artifacts.sh
@@ -2,7 +2,7 @@
 
 URL="https://callstack-github-bot.herokuapp.com/comment"
 LINK="https://$CIRCLE_BUILD_NUM-92112844-gh.circle-artifacts.com/$CIRCLE_NODE_INDEX/coverage/lcov-report/index.html"
-TEMPLATE="Hey @{{user.login}}, thank you for your pull request 🤗.\nThe coverage report for this branch can be viewed [here]($LINK)"
+TEMPLATE="Hey @{{user.login}}, thank you for your pull request 🤗.\nThe coverage report for this branch can be viewed [here]($LINK)."
 
 read -r -d '' DATA << EOM
 {
