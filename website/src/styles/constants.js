64119bb3092177d22c06a6bd9a5d577486cf2d45
Satyajit Sahoo <satyajit.happy@gmail.com>
2019-02-08 19:15:09 +0100

    docs: document how to use global styles (#306)

--- website/src/styles/constants.js
+++ website/src/styles/constants.js
@@ -1,7 +1,8 @@
 /* @flow */
 
 export default {
-  fontFamily: 'Montserrat, Helvetica, Arial, sans-serif',
+  fontFamily:
+    '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", Helvetica, sans-serif',
   gradient:
     'linear-gradient(to right, hsl(180, 100%, 70%), hsl(64, 57%, 82%), hsl(0, 100%, 90%))',
 };
