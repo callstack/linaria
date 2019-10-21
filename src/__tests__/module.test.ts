1b475997064ebc9ab807ad05e6b98e5fe1c10b51
Anton Evzhakov <anton@evz.name>
2019-10-21 20:51:20 +0300

    chore: move the evaluator to the plugin and improve performance

--- src/__tests__/module.test.ts
+++ src/__tests__/module.test.ts
@@ -1,21 +1,42 @@
-/* @flow */
-
 import path from 'path';
 import dedent from 'dedent';
 import * as babel from '@babel/core';
 import Module from '../babel/module';
+import { Evaluator, StrictOptions } from '../babel/types';
 
 beforeEach(() => Module.invalidate());
 
-function transform(text) {
-  return babel.transformSync(text, {
-    filename: this.filename,
-  });
+const evaluator: Evaluator = (filename, options, text) => {
+  const { code } = babel.transformSync(text, {
+    filename: filename,
+  })!;
+  return [code!, null];
+};
+
+function getFileName() {
+  return path.resolve(__dirname, `../__fixtures__/test.js`);
 }
+
+const options: StrictOptions = {
+  displayName: false,
+  evaluate: true,
+  rules: [
+    {
+      action: evaluator,
+    },
+    {
+      test: /\/node_modules\//,
+      action: 'ignore',
+    },
+  ],
+  babelOptions: {},
+};
+
+beforeEach(() => Module.invalidateEvalCache());
 
 it('creates module for JS files', () => {
   const filename = '/foo/bar/test.js';
-  const mod = new Module(filename);
+  const mod = new Module(filename, options);
 
   mod.evaluate('module.exports = () => 42');
 
@@ -36,7 +57,7 @@ it('correctly processes export declarations in strict mode', () => {
 });
 
 it('requires JS files', () => {
-  const mod = new Module(path.resolve(__dirname, '../__fixtures__/test.js'));
+  const mod = new Module(getFileName(), options);
 
   mod.evaluate(dedent`
     const answer = require('./sample-script');
@@ -48,7 +69,7 @@ it('requires JS files', () => {
 });
 
 it('requires JSON files', () => {
-  const mod = new Module(path.resolve(__dirname, '../__fixtures__/test.js'));
+  const mod = new Module(getFileName(), options);
 
   mod.evaluate(dedent`
     const data = require('./sample-data.json');
@@ -60,9 +81,8 @@ it('requires JSON files', () => {
 });
 
 it('imports JS files', () => {
-  const mod = new Module(path.resolve(__dirname, '../__fixtures__/test.js'));
+  const mod = new Module(getFileName(), options);
 
-  mod.transform = transform;
   mod.evaluate(dedent`
     import answer from './sample-script';
 
@@ -73,9 +93,11 @@ it('imports JS files', () => {
 });
 
 it('imports TypeScript files', () => {
-  const mod = new Module(path.resolve(__dirname, '../__fixtures__/test.ts'));
+  const mod = new Module(
+    path.resolve(__dirname, '../__fixtures__/test.ts'),
+    options
+  );
 
-  mod.transform = transform;
   mod.evaluate(dedent`
     import answer from './sample-typescript';
 
@@ -86,9 +108,8 @@ it('imports TypeScript files', () => {
 });
 
 it('imports JSON files', () => {
-  const mod = new Module(path.resolve(__dirname, '../__fixtures__/test.js'));
+  const mod = new Module(getFileName(), options);
 
-  mod.transform = transform;
   mod.evaluate(dedent`
     import data from './sample-data.json';
 
@@ -103,50 +124,51 @@ it('imports JSON files', () => {
 it('returns module from the cache', () => {
   /* eslint-disable no-self-compare */
 
-  const filename = path.resolve(__dirname, '../__fixtures__/test.js');
-  const mod = new Module(filename);
+  const filename = getFileName();
+  const mod = new Module(filename, options);
   const id = './sample-data.json';
 
   expect(mod.require(id) === mod.require(id)).toBe(true);
 
   expect(
-    new Module(filename).require(id) === new Module(filename).require(id)
+    new Module(filename, options).require(id) ===
+      new Module(filename, options).require(id)
   ).toBe(true);
 });
 
 it('clears modules from the cache', () => {
-  const filename = path.resolve(__dirname, '../__fixtures__/test.js');
+  const filename = getFileName();
   const id = './sample-data.json';
 
-  const result = new Module(filename).require(id);
+  const result = new Module(filename, options).require(id);
 
-  expect(result === new Module(filename).require(id)).toBe(true);
+  expect(result === new Module(filename, options).require(id)).toBe(true);
 
   Module.invalidate();
 
-  expect(result === new Module(filename).require(id)).toBe(false);
+  expect(result === new Module(filename, options).require(id)).toBe(false);
 });
 
 it('exports the path for non JS/JSON files', () => {
-  const mod = new Module(path.resolve(__dirname, '../__fixtures__/test.js'));
+  const mod = new Module(getFileName(), options);
 
   expect(mod.require('./sample-asset.png')).toBe('./sample-asset.png');
 });
 
 it('returns module when requiring mocked builtin node modules', () => {
-  const mod = new Module(path.resolve(__dirname, '../__fixtures__/test.js'));
+  const mod = new Module(getFileName(), options);
 
   expect(mod.require('path')).toBe(require('path'));
 });
 
 it('returns null when requiring empty builtin node modules', () => {
-  const mod = new Module(path.resolve(__dirname, '../__fixtures__/test.js'));
+  const mod = new Module(getFileName(), options);
 
   expect(mod.require('fs')).toBe(null);
 });
 
 it('throws when requiring unmocked builtin node modules', () => {
-  const mod = new Module(path.resolve(__dirname, '../__fixtures__/test.js'));
+  const mod = new Module(getFileName(), options);
 
   expect(() => mod.require('perf_hooks')).toThrow(
     'Unable to import "perf_hooks". Importing Node builtins is not supported in the sandbox.'
@@ -154,7 +176,7 @@ it('throws when requiring unmocked builtin node modules', () => {
 });
 
 it('has access to the global object', () => {
-  const mod = new Module(path.resolve(__dirname, '../__fixtures__/test.js'));
+  const mod = new Module(getFileName(), options);
 
   expect(() =>
     mod.evaluate(dedent`
@@ -164,17 +186,17 @@ it('has access to the global object', () => {
 });
 
 it("doesn't have access to the process object", () => {
-  const mod = new Module(path.resolve(__dirname, '../__fixtures__/test.js'));
+  const mod = new Module(getFileName(), options);
 
   expect(() =>
     mod.evaluate(dedent`
-    process.abort();
+    module.exports = process.abort();
   `)
   ).toThrow('process.abort is not a function');
 });
 
 it('has access to NODE_ENV', () => {
-  const mod = new Module(path.resolve(__dirname, '../__fixtures__/test.js'));
+  const mod = new Module(getFileName(), options);
 
   mod.evaluate(dedent`
   module.exports = process.env.NODE_ENV;
@@ -184,7 +206,7 @@ it('has access to NODE_ENV', () => {
 });
 
 it('has require.resolve available', () => {
-  const mod = new Module(path.resolve(__dirname, '../__fixtures__/test.js'));
+  const mod = new Module(getFileName(), options);
 
   mod.evaluate(dedent`
   module.exports = require.resolve('./sample-script');
@@ -196,7 +218,7 @@ it('has require.resolve available', () => {
 });
 
 it('has require.ensure available', () => {
-  const mod = new Module(path.resolve(__dirname, '../__fixtures__/test.js'));
+  const mod = new Module(getFileName(), options);
 
   expect(() =>
     mod.evaluate(dedent`
@@ -206,7 +228,7 @@ it('has require.ensure available', () => {
 });
 
 it('has __filename available', () => {
-  const mod = new Module(path.resolve(__dirname, '../__fixtures__/test.js'));
+  const mod = new Module(getFileName(), options);
 
   mod.evaluate(dedent`
   module.exports = __filename;
@@ -216,7 +238,7 @@ it('has __filename available', () => {
 });
 
 it('has __dirname available', () => {
-  const mod = new Module(path.resolve(__dirname, '../__fixtures__/test.js'));
+  const mod = new Module(getFileName(), options);
 
   mod.evaluate(dedent`
   module.exports = __dirname;
@@ -230,7 +252,7 @@ it('changes resolve behaviour on overriding _resolveFilename', () => {
 
   Module._resolveFilename = id => (id === 'foo' ? 'bar' : id);
 
-  const mod = new Module(path.resolve(__dirname, '../__fixtures__/test.js'));
+  const mod = new Module(getFileName(), options);
 
   mod.evaluate(dedent`
   module.exports = [
