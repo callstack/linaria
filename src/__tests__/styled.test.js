4c3179c9cfa67249b2cf16b6cb1ba575e7e97e66
Alexey Vasiliev <le0pard@users.noreply.github.com>
2019-10-22 14:09:02 +0300

    docs: add information how to extract className for components (#489)
    * feat: add linariaClassName for components for compositions
    
    * Extract linariaClassName from component as last item. Added test, so order will be not broken accidentally

--- src/__tests__/styled.test.js
+++ src/__tests__/styled.test.js
@@ -212,6 +212,35 @@ it('does not filter attributes for components', () => {
   expect(tree.toJSON()).toMatchSnapshot();
 });
 
+it('provides linaria component className for composition as last item in props.className', () => {
+  const Custom = props => {
+    const classnames = props.className.split(' ');
+    const linariaClassName = classnames[classnames.length - 1];
+    const newClassNames = [
+      props.className,
+      `${linariaClassName}--primary`,
+      `${linariaClassName}--accessibility`,
+    ].join(' ');
+
+    return (
+      <div className={newClassNames}>
+        original classname used for composition
+      </div>
+    );
+  };
+
+  const Test = styled(Custom)({
+    name: 'TestComponent',
+    class: 'abcdefg',
+  });
+
+  const tree = renderer.create(
+    <Test className="some-another-class">This is a test</Test>
+  );
+
+  expect(tree.toJSON()).toMatchSnapshot();
+});
+
 it('throws when using as tag for template literal', () => {
   expect(
     () =>
