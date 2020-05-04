6c45beed9564a0b8264bfd619512152e0afd91c8
Tomas Carnecky <tomc@caurea.org>
2020-05-04 13:53:08 +0200

    Make linaria/react available as ESM

--- babel.config.js
+++ babel.config.js
@@ -14,6 +14,18 @@ module.exports = {
         ],
       ],
     },
+    test: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              node: '10',
+            },
+          },
+        ],
+      ],
+    },
   },
   overrides: [
     {
@@ -47,6 +59,30 @@ module.exports = {
             ],
           ],
         },
+        test: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  browsers: [
+                    'last 2 versions',
+                    'not ie 11',
+                    'not ie_mob 11',
+                    'not op_mini all',
+                    'not dead',
+                  ],
+                },
+                loose: true,
+                // our styled component should not need to use any polyfill. We do not include core-js in dependencies. However, we leave this to detect if future changes would not introduce any need for polyfill
+                useBuiltIns: 'usage',
+                corejs: 3,
+                // this is used to test if we do not introduced core-js polyfill
+                debug: process.env.NODE_ENV === 'debug',
+              },
+            ],
+          ],
+        },
       },
     },
     {
@@ -57,6 +93,25 @@ module.exports = {
       presets: ['@babel/preset-react'],
       env: {
         legacy: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  browsers: [
+                    'last 2 versions',
+                    'not ie 11',
+                    'not ie_mob 11',
+                    'not op_mini all',
+                    'not dead',
+                  ],
+                },
+                useBuiltIns: false,
+              },
+            ],
+          ],
+        },
+        test: {
           presets: [
             [
               '@babel/preset-env',
