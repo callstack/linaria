bf3276b5e380b735ce5d1a656deed3e963ed79b3
tsubasa1218 <juan.suaza1218@gmail.com>
2020-05-04 21:21:40 -0400

    docs: add linaria example to main file for preact example

--- examples/Preact/preact-example/src/components/app.js
+++ examples/Preact/preact-example/src/components/app.js
@@ -3,9 +3,20 @@ import { Router } from 'preact-router';
 
 import Header from './header';
 
+import { css } from 'linaria';
+import { styled } from 'linaria/react';
+
 // Code-splitting is automated for routes
 import Home from '../routes/home';
 import Profile from '../routes/profile';
+
+const Background = styled.div`
+    background-color: aliceblue;
+`; 
+
+const bluishText = css`
+    color: cornflowerblue;
+`
 
 export default class App extends Component {
 	
@@ -19,14 +30,14 @@ export default class App extends Component {
 
 	render() {
 		return (
-			<div id="app">
+			<Background class={bluishText} id="app">
 				<Header />
 				<Router onChange={this.handleRoute}>
 					<Home path="/" />
 					<Profile path="/profile/" user="me" />
 					<Profile path="/profile/:user" />
 				</Router>
-			</div>
+			</Background>
 		);
 	}
 }
