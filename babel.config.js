7c335b9b30bbb32bb9888e22fa0c3e47327f09c8
Tomas Carnecky <tomc@caurea.org>
2020-05-08 13:14:04 +0200

    Attempt to clean up babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,30 @@
+/*
+ * The following environments are supported:
+ *
+ * (unspecified): Produces ES module output, no language features
+ *   (except non-standard ones) are transpiled.
+ * "legacy": Produces CommonJS output, uses @babel/preset-env to target
+ *   Node.js 10 and specific browsers.
+ * "test": Used by Jest, produces CommonJS output, targetting
+ *   the current Node.js version.
+ */
+
+/*
+ * Configuration for the legacy build
+ */
+const legacy = {
+  targets: {
+    node: '10',
+    browsers: [
+      'last 2 versions',
+      'not ie 11',
+      'not ie_mob 11',
+      'not op_mini all',
+      'not dead',
+    ],
+  },
+};
+
 module.exports = {
   presets: ['@babel/preset-typescript'],
   plugins: ['@babel/plugin-proposal-class-properties'],
@@ -8,7 +35,7 @@ module.exports = {
           '@babel/preset-env',
           {
             targets: {
-              node: '10',
+              node: legacy.targets.node,
             },
           },
         ],
@@ -20,7 +47,7 @@ module.exports = {
           '@babel/preset-env',
           {
             targets: {
-              node: '10',
+              node: 'current',
             },
           },
         ],
@@ -41,37 +68,25 @@ module.exports = {
               '@babel/preset-env',
               {
                 targets: {
-                  browsers: [
-                    'last 2 versions',
-                    'not ie 11',
-                    'not ie_mob 11',
-                    'not op_mini all',
-                    'not dead',
-                  ],
+                  browsers: legacy.targets.browsers,
                 },
                 loose: true,
-                // our styled component should not need to use any polyfill. We do not include core-js in dependencies. However, we leave this to detect if future changes would not introduce any need for polyfill
-                useBuiltIns: 'usage',
-                corejs: 3,
-                // this is used to test if we do not introduced core-js polyfill
-                debug: process.env.NODE_ENV === 'debug',
               },
             ],
           ],
         },
         test: {
+          /*
+           * This configuration is basically the same as for the "legacy" env,
+           * except that we enable certain options so we can track whether
+           * core-js polyfills have been inserted or not.
+           */
           presets: [
             [
               '@babel/preset-env',
               {
                 targets: {
-                  browsers: [
-                    'last 2 versions',
-                    'not ie 11',
-                    'not ie_mob 11',
-                    'not op_mini all',
-                    'not dead',
-                  ],
+                  browsers: legacy.targets.browsers,
                 },
                 loose: true,
                 // our styled component should not need to use any polyfill. We do not include core-js in dependencies. However, we leave this to detect if future changes would not introduce any need for polyfill
@@ -90,47 +105,17 @@ module.exports = {
        * we use the same config for src/__tests__ and src/__fixtures__ to not break existing tests
        */
       test: /src\/((__tests__)|(__fixtures__))\//,
-      presets: ['@babel/preset-react'],
-      env: {
-        legacy: {
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                targets: {
-                  browsers: [
-                    'last 2 versions',
-                    'not ie 11',
-                    'not ie_mob 11',
-                    'not op_mini all',
-                    'not dead',
-                  ],
-                },
-                useBuiltIns: false,
-              },
-            ],
-          ],
-        },
-        test: {
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                targets: {
-                  browsers: [
-                    'last 2 versions',
-                    'not ie 11',
-                    'not ie_mob 11',
-                    'not op_mini all',
-                    'not dead',
-                  ],
-                },
-                useBuiltIns: false,
-              },
-            ],
-          ],
-        },
-      },
+      presets: [
+        '@babel/preset-react',
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              browsers: legacy.targets.browsers,
+            },
+          },
+        ],
+      ],
     },
   ],
 };
