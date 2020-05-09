26e9d8c8a3db31ae45bcad4b0c0c9eb8b9b66290
Anton Evzhakov <anton@evz.name>
2020-05-09 17:52:57 +0300

    feat(evaluators): make evaluators exportable from the root

---
+++ evaluators.js
@@ -0,0 +1,7 @@
+const extractor = require('./lib/babel/evaluators/extractor').default;
+const shaker = require('./lib/babel/evaluators/shaker').default;
+
+module.exports = {
+  extractor,
+  shaker,
+};
