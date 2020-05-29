95b6c4728e15744b64cb2e591909447d085e3dfb
Tomasz Krzyżowski <tomasz.krzyzowski@callstack.com>
2020-05-29 13:09:19 +0200

    Transformer returns always transformed code if file contains only unused linaria code (#626)
    * Transformer returns always transformed code if file contains only unused linaria code
    
    * Fixed source map type issue
    
    * Added timeout to core-js test
    
    * Adjusted test name

--- src/__tests__/transform.test.ts
+++ src/__tests__/transform.test.ts
@@ -212,3 +212,24 @@ it("doesn't throw due to duplicate preset", async () => {
     )
   ).not.toThrow('Duplicate plugin/preset detected');
 });
+
+it('should return transformed code even when file only contains unused linaria code', async () => {
+  const { code } = await transform(
+    dedent`
+    import { css } from 'linaria';
+
+    const title = css\`
+      color: red;
+    \`;
+    `,
+    {
+      filename: './test.js',
+      outputFilename,
+      pluginOptions: {
+        rules,
+      },
+    }
+  );
+
+  expect(code).not.toContain('css`');
+});
--- src/transform.ts
+++ src/transform.ts
@@ -86,8 +86,13 @@ export default function transform(code: string, options: Options): Result {
       .linaria
   ) {
     return {
-      code,
-      sourceMap: options.inputSourceMap,
+      code: transformedCode || '', // if there was only unused code we want to return transformed code which will be later removed by the bundler
+      sourceMap: map
+        ? {
+            ...map,
+            version: map.version.toString(),
+          }
+        : null,
     };
   }
 
