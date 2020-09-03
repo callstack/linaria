62530758f2860c1fde700c1ee6393b6ef3c5676c
Anton Evzhakov <anton@evz.name>
2020-09-03 14:15:04 +0300

    Update collect.ts

--- src/server/collect.ts
+++ src/server/collect.ts
@@ -101,7 +101,7 @@ const extractClassesFromHtml = (html: string): RegExp => {
   let match = regex.exec(html);
 
   while (match !== null) {
-    match[1].split(' ').forEach(className => {
+    match[1].split(' ').forEach((className) => {
       className = className.replace(
         /\\|\^|\$|\{|\}|\[|\]|\(|\)|\.|\*|\+|\?|\|/g,
         '\\$&'
