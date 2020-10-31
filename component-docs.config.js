350a8a0acbde04e438d9f7a9dfeed1870c66a7da
Thanh Tran <thanhtran@chotot.vn>
2020-10-31 18:12:12 +0700

    chore: upgrade component-docs to latest with explicit config file

---
+++ component-docs.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  port: 3031,
+  /* prettier-ignore */
+  pages: [
+    { type: 'mdx', file: path.resolve(__dirname, 'docs/00.HOME.mdx') },
+    { type: 'md', file: path.resolve(__dirname, 'docs/01.GET_STARTED.md') },
+    { type: 'md', file: path.resolve(__dirname, 'docs/02.BASICS.md')},
+    { type: 'md', file: path.resolve(__dirname, 'docs/03.BENEFITS.md')},
+    { type: 'md', file: path.resolve(__dirname, 'docs/04.API.md')},
+    { type: 'md', file: path.resolve(__dirname, 'docs/05.CONFIGURATION.md')},
+    { type: 'md', file: path.resolve(__dirname, 'docs/06.CLI.md')},
+    { type: 'md', file: path.resolve(__dirname, 'docs/07.BUNDLERS_INTEGRATION.md')},
+    { type: 'md', file: path.resolve(__dirname, 'docs/08.LINTING.md')},
+    { type: 'md', file: path.resolve(__dirname, 'docs/09.CRITICAL_CSS.md')},
+    { type: 'md', file: path.resolve(__dirname, 'docs/10.DYNAMIC_STYLES.md')},
+    { type: 'md', file: path.resolve(__dirname, 'docs/11.THEMING.md')},
+    { type: 'md', file: path.resolve(__dirname, 'docs/12.HOW_IT_WORKS.md')},
+    { type: 'md', file: path.resolve(__dirname, 'docs/13.2.0_MIGRATION_GUIDE.md')},
+  ]
+};
