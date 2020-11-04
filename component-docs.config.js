e767e291b3483d22d805e32526a86db066282955
Thanh Tran <thanhtran@chotot.vn>
2020-11-04 15:48:51 +0700

    refactor: remove order number from markdowns, more component-docs config

--- component-docs.config.js
+++ component-docs.config.js
@@ -1,24 +1,44 @@
 const path = require('path');
 
+// pages structure and order
+const pages = [
+  'docs/HOME.mdx',
+  'docs/GET_STARTED.md',
+  'docs/BASICS.md',
+  'docs/BENEFITS.md',
+  '---',
+  'docs/API.md',
+  'docs/CONFIGURATION.md',
+  'docs/CLI.md',
+  'docs/BUNDLERS_INTEGRATION.md',
+  'docs/LINTING.md',
+  'docs/CRITICAL_CSS.md',
+  'docs/DYNAMIC_STYLES.md',
+  'docs/THEMING.md',
+  'docs/HOW_IT_WORKS.md',
+  '---',
+  'docs/2.0_MIGRATION_GUIDE.md',
+];
+
+const extMap = {
+  md: 'md',
+  mdx: 'mdx',
+  js: 'component',
+};
+
 module.exports = {
   port: 3031,
-  assets: [path.resolve(__dirname, 'website/assets')],
+  root: path.resolve(__dirname),
+  assets: ['website/assets'],
   logo: '/assets/linaria-logo-colored.svg',
-  /* prettier-ignore */
-  pages: [
-    { type: 'mdx', file: path.resolve(__dirname, 'docs/00.HOME.mdx') },
-    { type: 'md', file: path.resolve(__dirname, 'docs/01.GET_STARTED.md') },
-    { type: 'md', file: path.resolve(__dirname, 'docs/02.BASICS.md')},
-    { type: 'md', file: path.resolve(__dirname, 'docs/03.BENEFITS.md')},
-    { type: 'md', file: path.resolve(__dirname, 'docs/04.API.md')},
-    { type: 'md', file: path.resolve(__dirname, 'docs/05.CONFIGURATION.md')},
-    { type: 'md', file: path.resolve(__dirname, 'docs/06.CLI.md')},
-    { type: 'md', file: path.resolve(__dirname, 'docs/07.BUNDLERS_INTEGRATION.md')},
-    { type: 'md', file: path.resolve(__dirname, 'docs/08.LINTING.md')},
-    { type: 'md', file: path.resolve(__dirname, 'docs/09.CRITICAL_CSS.md')},
-    { type: 'md', file: path.resolve(__dirname, 'docs/10.DYNAMIC_STYLES.md')},
-    { type: 'md', file: path.resolve(__dirname, 'docs/11.THEMING.md')},
-    { type: 'md', file: path.resolve(__dirname, 'docs/12.HOW_IT_WORKS.md')},
-    { type: 'md', file: path.resolve(__dirname, 'docs/13.2.0_MIGRATION_GUIDE.md')},
-  ]
+  output: 'docs-public',
+  pages: pages.map((page) => {
+    if (page === '---') return { type: 'separator' };
+
+    return {
+      type: extMap[path.extname(page).substr(1)],
+      file: path.resolve(__dirname, page),
+    };
+  }),
+  title: '[title] - Linaria Documentation',
 };
