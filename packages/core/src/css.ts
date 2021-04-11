50ea2d593f6a0f3c997b0741173627a093279c29
Anton Evzhakov <anton@evz.name>
2021-04-11 15:11:27 +0300

    fix(core): remove unnecessary spread operators from css and cx (#746)

--- packages/core/src/css.ts
+++ packages/core/src/css.ts
@@ -1,11 +1,15 @@
 import type { CSSProperties } from './CSSProperties';
 import type { StyledMeta } from './StyledMeta';
 
-export default function css(
-  _strings: TemplateStringsArray,
-  ..._exprs: Array<string | number | CSSProperties | StyledMeta>
-): string {
+type CSS = (
+  strings: TemplateStringsArray,
+  ...exprs: Array<string | number | CSSProperties | StyledMeta>
+) => string;
+
+const css: CSS = () => {
   throw new Error(
     'Using the "css" tag in runtime is not supported. Make sure you have set up the Babel plugin correctly.'
   );
-}
+};
+
+export default css;
