3f07236d026b3ab87b0fa4f825eabc20b0300eb8
Anber <anton@evz.name>
2021-11-29 14:55:18 +0000

    Deploying to gh-pages from  @ 4773bcf4b14f08cdc4d2b612654b962cdfc97eaa 🚀

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,22 @@
 /* @flow */
 
-import { styled } from '@linaria/react';
 import React from 'react';
+import { css, cx } from '@linaria/atomic';
 import Header from './Header';
 import Hero from './Hero';
 
-export default function Index() {
-  return (
-    <Page>
-      <Header />
-      <Hero />
-    </Page>
-  );
-}
-
-const Page = styled.div`
+const Page = css`
   background: linear-gradient(to bottom right, #b24592, #f15f79);
   color: #fff;
   min-height: 100vh;
   text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.08);
 `;
+
+export default function Index() {
+  return (
+    <div className={cx(Page)}>
+      <Header />
+      <Hero />
+    </div>
+  );
+}
