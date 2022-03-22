c814ff5ee78e6f82e14c2b1bd1206f32a73a4cf4
Anber <anton@evz.name>
2022-03-22 10:53:06 +0000

    Deploying to gh-pages from  @ c44becb11b2eec795b68c2b3d0715672ba4b3888 🚀

--- node_modules/urijs/src/IPv6.js
+++ node_modules/urijs/src/IPv6.js
@@ -2,7 +2,7 @@
  * URI.js - Mutating URLs
  * IPv6 Support
  *
- * Version: 1.19.7
+ * Version: 1.19.10
  *
  * Author: Rodney Rehm
  * Web: http://medialize.github.io/URI.js/
--- node_modules/urijs/src/URITemplate.js
+++ node_modules/urijs/src/URITemplate.js
@@ -2,7 +2,7 @@
  * URI.js - Mutating URLs
  * URI Template Support - http://tools.ietf.org/html/rfc6570
  *
- * Version: 1.19.7
+ * Version: 1.19.10
  *
  * Author: Rodney Rehm
  * Web: http://medialize.github.io/URI.js/
