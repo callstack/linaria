ae5d66c79c219ad9d3c1890172d4716bc2c969d0
Anber <anton@evz.name>
2022-06-09 18:35:25 +0000

    Deploying to gh-pages from  @ ab2ac858386c0a3bcea29530fd31c1b189175eee 🚀

---
+++ build/components/Example.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Page = exports.AtomicPage = void 0;
+
+var _core = require("@linaria/core");
+
+var _atomic = require("@linaria/atomic");
+
+const Page = "p17oew5k";
+exports.Page = Page;
+const AtomicPage = "atm_26_1u0dmv0 atm_7l_u67f1s atm_j6_1kxcs5u atm_sy_12yic89";
+exports.AtomicPage = AtomicPage;
