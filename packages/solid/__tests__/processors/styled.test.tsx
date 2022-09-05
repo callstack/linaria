29e5fd41665539cd267ee6302c2fb09018402e59
raveclassic <raveclassic@gmail.com>
2022-09-05 12:51:02 +0300

    build(solid): migrate to babel-preset-jest

--- packages/solid/__tests__/processors/styled.test.tsx
+++ packages/solid/__tests__/processors/styled.test.tsx
@@ -1,7 +1,40 @@
-import { styled } from '../../src/styled';
+import * as babel from '@babel/core';
+// import {} from '@linaria/babel-preset';
+import dedent from 'dedent';
+
+import { styled } from '@linaria/solid';
+
+function transform(source: string): string {
+  const result = babel.transform(source, {
+    babelrc: false,
+    configFile: false,
+    filename: 'test.tsx',
+    presets: ['@linaria/babel-preset'],
+  });
+  if (!result || !result.code)
+    throw new Error('Babel cannot parse source code');
+  return dedent(result.code.trim());
+}
 
 describe('styled processor', () => {
   it('renders tag', () => {
-    const Test = styled.div``;
+    // const result = transform(`
+    //   import { styled } from '@linaria/solid'
+    //   export const Component = styled.div\`\`
+    // `);
+    // expect(result).toEqual(
+    //   dedent(`
+    //   export const Component = props => {
+    //     const className = "cg10ziu" + (props.class ? " " + props.class : "");
+    //     const style = props.style;
+    //     return <div {...props} class={className} style={style} />;
+    //   };
+    // `)
+    // );
+    // const Component = styled.div`
+    //   color: blue;
+    // `;
+    // console.log(Component);
+    console.log(styled);
   });
 });
---
+++ packages/solid/jest.config.js
@@ -0,0 +1,5 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'babel-jest',
+  testEnvironment: 'node',
+};
