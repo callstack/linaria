091496cbbd4533b2e375fef6f3d1cb59813f6960
Anton Evzhakov <anton@evz.name>
2022-10-19 21:37:47 +0300

    fix(atomic,core,esbuild,react,rollup): ES module

--- examples/vite/vite.config.js
+++ examples/vite/vite.config.js
@@ -12,6 +12,9 @@ export default defineConfig(({ command }) => ({
     {
       ...linaria({
         include: ['**/*.{js,jsx}'],
+        babelOptions: {
+          presets: ['@babel/preset-react'],
+        },
       }),
       enforce: 'pre',
     },
