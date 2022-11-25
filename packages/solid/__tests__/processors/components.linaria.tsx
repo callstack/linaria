389ff6493a08cf86aad6f267ad486635639cba48
raveclassic <raveclassic@gmail.com>
2022-11-25 18:15:37 +0300

    test(solid): drop tests

--- packages/solid/__tests__/processors/components.linaria.tsx
+++
@@ -1,9 +0,0 @@
-import { styled } from '../../src';
-
-interface TagProps {
-  readonly bg?: string;
-}
-export const Tag = styled.div<TagProps>`
-  color: red;
-  background: ${(props) => props.bg};
-`;
