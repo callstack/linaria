77bcf2e729cf9c681477077f07200f7dbc3b99e7
Ezlie Nguyen <ezlie.nguyen@gmail.com>
2022-12-20 11:07:51 -0500

    feat: add ignored and excluded classes to critical CSS extraction in `collect` (#1102)
    * feat: add ignored and blocked classes lists to collect
    
    * feat: add exclusion functionality in handleAtRule
    
    * chore(changeset): add changeset for collect updates
    
    Co-authored-by: Ezlie Nguyen <ezlie.nguyen@airbnb.com>

--- packages/server/src/collect.ts
+++ packages/server/src/collect.ts
@@ -1,7 +1,3 @@
-/**
- * This utility extracts critical CSS from given HTML and CSS file to be used in SSR environments
- */
-
 import type { AtRule, ChildNode } from 'postcss';
 import postcss from 'postcss';
 
@@ -10,19 +6,35 @@ type CollectResult = {
   other: string;
 };
 
-const extractClassesFromHtml = (html: string): RegExp => {
+interface ClassnameModifiers {
+  ignoredClasses?: string[];
+  blockedClasses?: string[];
+}
+
+/**
+ * Used to escape `RegExp`
+ * [syntax characters](https://262.ecma-international.org/7.0/#sec-regular-expressions-patterns).
+ */
+function escapeRegex(string: string) {
+  return string.replace(/[\\^$.*+?()[\]{}|]/g, '\\$&');
+}
+
+const extractClassesFromHtml = (
+  html: string,
+  ignoredClasses: string[]
+): RegExp => {
   const htmlClasses: string[] = [];
   const regex = /\s+class="([^"]+)"/gm;
   let match = regex.exec(html);
+  const ignoredClassesDeduped = new Set(ignoredClasses);
 
   while (match !== null) {
     match[1].split(' ').forEach((className) => {
       // eslint-disable-next-line no-param-reassign
-      className = className.replace(
-        /\\|\^|\$|\{|\}|\[|\]|\(|\)|\.|\*|\+|\?|\|/g,
-        '\\$&'
-      );
-      htmlClasses.push(className);
+      className = escapeRegex(className);
+      if (className !== '' && !ignoredClassesDeduped.has(className)) {
+        htmlClasses.push(className);
+      }
     });
     match = regex.exec(html);
   }
@@ -30,16 +42,40 @@ const extractClassesFromHtml = (html: string): RegExp => {
   return new RegExp(htmlClasses.join('|'), 'gm');
 };
 
-export default function collect(html: string, css: string): CollectResult {
+/**
+ * This utility extracts critical CSS from given HTML and CSS file to be used in SSR environments
+ * @param {string} html the HTML from which classes will be parsed
+ * @param {string} css the CSS file from which selectors will be parsed and determined as critical or other
+ * @param {string[]} ignoredClasses classes that, when present in the HTML, will not be included in the regular expression used to match selectors
+ * @param {string[]} blockedClasses classes that, when contained in a selector, will cause the selector to be marked as not critical
+ * @returns {CollectResult} object containing the critical and other CSS styles
+ */
+export default function collect(
+  html: string,
+  css: string,
+  classnameModifiers?: ClassnameModifiers
+): CollectResult {
   const animations = new Set();
   const other = postcss.root();
   const critical = postcss.root();
   const stylesheet = postcss.parse(css);
-  const htmlClassesRegExp = extractClassesFromHtml(html);
+  const ignoredClasses = classnameModifiers?.ignoredClasses ?? [];
+  const blockedClasses = classnameModifiers?.blockedClasses ?? [];
+
+  const htmlClassesRegExp = extractClassesFromHtml(html, ignoredClasses);
+  const blockedClassesSanitized = blockedClasses.map(escapeRegex);
+  const blockedClassesRegExp = new RegExp(
+    blockedClassesSanitized.join('|'),
+    'gm'
+  );
 
   const isCritical = (rule: ChildNode) => {
     // Only check class names selectors
     if ('selector' in rule && rule.selector.startsWith('.')) {
+      const isExcluded =
+        blockedClasses.length > 0 && blockedClassesRegExp.test(rule.selector);
+      if (isExcluded) return false;
+
       return Boolean(rule.selector.match(htmlClassesRegExp));
     }
 
@@ -47,23 +83,30 @@ export default function collect(html: string, css: string): CollectResult {
   };
 
   const handleAtRule = (rule: AtRule) => {
-    let addedToCritical = false;
-
-    rule.each((childRule) => {
-      if (isCritical(childRule) && !addedToCritical) {
-        critical.append(rule.clone());
-        addedToCritical = true;
-      }
-    });
-
     if (rule.name === 'keyframes') {
       return;
     }
 
-    if (addedToCritical) {
-      rule.remove();
-    } else {
-      other.append(rule);
+    const criticalRule = rule.clone();
+    const otherRule = rule.clone();
+
+    let removedNodesFromOther = 0;
+    criticalRule.each((childRule: ChildNode, index: number) => {
+      if (isCritical(childRule)) {
+        otherRule.nodes[index - removedNodesFromOther]?.remove();
+        removedNodesFromOther += 1;
+      } else {
+        childRule.remove();
+      }
+    });
+
+    rule.remove();
+
+    if (criticalRule.nodes.length > 0) {
+      critical.append(criticalRule);
+    }
+    if (otherRule.nodes.length > 0) {
+      other.append(otherRule);
     }
   };
 
