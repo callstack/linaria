61d49a39fee6c1c37464dd2666b58ccb6d3a94a0
yepitschunked <125177+yepitschunked@users.noreply.github.com>
2023-02-06 02:06:23 -0800

    fix(babel): lookup referencedIdentifiers after tree mutations (#1180)
    * fix: lookup referencedIdentifiers after tree mutations
    
    * fix(babel): lookup referencedIdentifiers after tree mutations (fixes #1112)
    
    ---------
    
    Co-authored-by: Victor Lin <victor.lin@airbnb.com>
    Co-authored-by: Anton Evzhakov <anton@evz.name>

---
+++ .changeset/yellow-cougars-invite.md
@@ -0,0 +1,6 @@
+---
+'@linaria/utils': patch
+'@linaria/babel-preset': patch
+---
+
+Fix for #1112 "Cannot read properties of undefined (reading 'localeCompare')"
--- packages/utils/src/scopeHelpers.ts
+++ packages/utils/src/scopeHelpers.ts
@@ -369,6 +369,10 @@ function removeWithRelated(paths: NodePath[]) {
   const affectedPaths = actions.map(getPathFromAction);
 
   let referencedIdentifiers = findIdentifiers(affectedPaths, 'referenced');
+  referencedIdentifiers.sort((a, b) =>
+    a.node?.name.localeCompare(b.node?.name)
+  );
+
   const referencesOfBinding = findIdentifiers(affectedPaths, 'binding')
     .map((i) => (i.node && getScope(i).getBinding(i.node.name)) ?? null)
     .filter(isNotNull)
@@ -390,10 +394,6 @@ function removeWithRelated(paths: NodePath[]) {
   });
 
   removeWithRelated(referencesOfBinding);
-
-  referencedIdentifiers.sort((a, b) =>
-    a.node?.name.localeCompare(b.node?.name)
-  );
 
   let clean = false;
   while (!clean && referencedIdentifiers.length > 0) {
