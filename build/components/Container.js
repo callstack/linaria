95e5702b8fd2bb1dcb592b39761c8d33da0cab94
Anber <anton@evz.name>
2023-09-26 14:38:16 +0000

    Deploying to gh-pages from  @ 25ba13444d04cf306401cfb0cfa2bed27f13897f 🚀

---
+++ build/components/Container.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _react = require("@linaria/react");
+const Container = /*#__PURE__*/(0, _react.styled)('div')({
+  name: "Container",
+  class: "cvg8y9q",
+  propsAsIs: false
+});
+var _default = Container;
+exports.default = _default;
