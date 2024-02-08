10bd626b9b09c8dd52a1a0bdcdf793a7c647acbe
Anber <anton@evz.name>
2024-02-08 12:09:43 +0000

    Deploying to gh-pages from  @ 049a4ccb77e29f3628353352db21bd446fa04a2e 🚀

---
+++ build/styles/constants.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _default = exports.default = {
+  fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", Helvetica, sans-serif',
+  gradient: 'linear-gradient(to right, hsl(180, 100%, 70%), hsl(64, 57%, 82%), hsl(0, 100%, 90%))'
+};
